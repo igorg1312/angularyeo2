d5f35042a435c03b9bf4bd3338c728638d712028
Graham Percy <igorgarofano@gmail.com>
2016-05-12 15:43:25 +0200

    first

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scrivaniaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scrivaniaApp
+ */
+angular.module('scrivaniaApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scrivaniaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scrivaniaApp
+ */
+angular.module('scrivaniaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
